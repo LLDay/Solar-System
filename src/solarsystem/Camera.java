4c05203d46a92c2071d42ce729678b80dba47fce
LLDay <ssdenis99@gmail.com>
2018-05-05 21:53:09 +0300

    Implemented moving of camera

--- src/solarsystem/Camera.java
+++ src/solarsystem/Camera.java
@@ -1,15 +1,26 @@
 package solarsystem;
 
+import java.awt.Dimension;
+import java.awt.MouseInfo;
 import java.awt.event.*;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Camera extends AffineTransform implements MouseWheelListener, MouseListener {
+import javax.swing.SwingUtilities;
+import javax.swing.event.MouseInputListener;
+
+public class Camera extends MouseAdapter
+implements MouseWheelListener, MouseListener  {
 	private double zoom;
 	private double posX;
 	private double posY;
+	private Dimension canvSize;
+	
+	private double speed = 0.05;
+	
+	private int mouseX;
+	private int mouseY;
 	private Timer timer;
 	
 	Camera() {
@@ -17,13 +28,9 @@ public class Camera extends AffineTransform implements MouseWheelListener, Mouse
 	}
 	
 	Camera(double prefZoom) {
-		timer = new Timer();
 		zoom = prefZoom; 
 	}
 	
-	public final double getZoom() {
-		return zoom;
-	}
 	
 	public void setZoom(double newZoom) {
 		if (newZoom < 0.0)
@@ -32,47 +39,87 @@ public class Camera extends AffineTransform implements MouseWheelListener, Mouse
 		zoom = newZoom;
 	}
 	
+	public double getZoom() {
+		return zoom;
+	}
+	
+	
+	public void setPosX(double position) {
+		posX = position;
+	}
+	
+	public double getPosX() {
+		return posX;
+	}
+	
+	
+	public void setPosY(double position) {
+		posY = position;
+	}
+	
+	public double getPosY() {
+		return posY;
+	}
+	
+	
+	public void setCanvasSize(final Dimension size) {
+		if (size == null)
+			throw new NullPointerException("Canvas size is null");
+		
+		canvSize = size;
+	}
+
+	public void setCenvasSize(int width, int height) {
+		if (width <= 0 || height <= 0)
+			throw new IllegalArgumentException("Wrong canvas size");
+		
+		canvSize = new Dimension(width, height);
+	}
+	
+	public final Dimension getCanvasSize() {
+		return canvSize;
+	}
+	
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
-		double zoomFactor = -0.01 * e.getPreciseWheelRotation() * zoom;
-        zoom = zoom + zoomFactor;
-        
-        
-        this.setToScale(zoom, zoom);
+		double zoomFactor = -0.1 * e.getPreciseWheelRotation() * zoom;
+        zoom += zoomFactor;
 	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {System.out.println("Klick");
-	this.setToTranslation(posX++, posY);}
-
-	@Override
-	public void mouseExited(MouseEvent e) {}
-
+	
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if(timer == null)
-        {
-			timer = new Timer();
-        }
-        timer.schedule(new TimerTask()
-        {
-            public void run()
-            {
-                System.out.println("Action");
-            }
-        }, 300, 500);
+		if (canvSize == null)
+			return;
 		
+		if (!SwingUtilities.isRightMouseButton(e))
+			return;
+		
+		if (timer == null)
+			timer = new java.util.Timer();
+		System.out.println(mouseX);
+		timer.schedule(new TimerTask() {
+            public void run() {
+            	posX += speed * (canvSize.getWidth() / 2 - mouseX);
+        		posY += speed * (canvSize.getHeight() / 2 - mouseY);
+            }
+        }, 0, 50);
 	}
 
+	
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		if(timer != null)
-        {
-			timer.cancel();
-			timer = null;
-        }	
+	public void mouseReleased(MouseEvent e)
+    {
+        if(timer != null) {
+        	timer.cancel();
+        	timer = null;
+        }
+    }
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		mouseX = e.getX();
+		mouseY = e.getY();
 	}
 }
+
--- src/solarsystem/SolarCanvas.java
+++ src/solarsystem/SolarCanvas.java
@@ -1,35 +1,50 @@
 package solarsystem;
 
 import java.awt.*;
-import java.awt.MultipleGradientPaint.CycleMethod;
-import java.awt.event.MouseWheelEvent;
+import java.awt.event.*;
 import java.awt.geom.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SolarCanvas extends Canvas {
 	private Camera camera;
 	private Sun sun;
 	
+	private int fps = 30;
+
+	
 	
 	public SolarCanvas(int solarRad, Color solarColor, int gradientRadius) {
 		camera = new Camera();
-		
 		sun = new Sun(200);		
 		
+		
 		addMouseWheelListener(camera);
-		addMouseWheelListener(e -> this.repaint());
+		addMouseMotionListener(camera);
 		addMouseListener(camera);
+		
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+            public void run() {
+            	repaint();
+            }
+        }, 100, 1000 / fps);
+	
 	}
 	
 	@Override
 	public void paint(Graphics g) {
 		Graphics2D graphics = (Graphics2D) g;
 		
-	
-		graphics.setTransform(camera);
+		camera.setCanvasSize(this.getSize());
 		
+		AffineTransform at = new AffineTransform();
+        at.translate(camera.getPosX(), camera.getPosY());
+        at.scale(camera.getZoom(), camera.getZoom());
+
+		graphics.setTransform(at);
 		graphics.setPaint(sun.getPaint());
 	    graphics.fill(sun.getShape());
-	  
 	}
 	
 
